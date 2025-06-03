1895c7d70c80221dcd5654d5290d1db471a5451c
celesc16 <celechoquevillca2004@gmail.com>
2025-06-02 23:14:00 -0300

    Se añade en el init.py los demas modelos

--- models/especialidad.py
+++ models/especialidad.py
@@ -4,4 +4,4 @@ from .base import Base
 class Especialidad(Base):
   __tablename__ = 'especialidades'
   especialidad = Column(Integer, primary_key=True, autoincrement=True)
-  nombre = Column(String(100), nullable=False, unique=True) 
+  nombre = Column(String(100), nullable=False) 
