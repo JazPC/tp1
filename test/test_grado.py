123b4aaf9879c03d02c8e2d7667aae567f50cbf5
celesc16 <celechoquevillca2004@gmail.com>
2025-06-03 17:27:33 -0300

    Se añade test de grado

--- test/test_grado.py
+++ test/test_grado.py
@@ -1,27 +1,55 @@
 import unittest
-from flask import current_app
-from config import create_app
-from app.models import Grado
+import tempfile
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.base import Base
+from utils.xml_importer import import_data
+from models import Grado
 
-class CartTestCase(unittest.TestCase):
-
+class TestImportGrados(unittest.TestCase):
     def setUp(self):
-        self.app = create_app()
-        self.app_context = self.app.app_context()
-        self.app_context.push()
+        #Crea bd en memoria
+        engine = create_engine("sqlite:///:memory:")
+        Base.metadata.create_all(engine)
+        self.Session = sessionmaker(bind=engine)
+
+        # Archivo temporal para simular el XML de entrada
+        self.temp = tempfile.NamedTemporaryFile(delete=False, suffix=".xml", mode="w", encoding="Windows-1252")
+        self.temp.write("""
+        <root>
+            <_expxml>
+                <grado>1</grado>
+                <nombre>Administración</nombre>
+            </_expxml>
+            <_expxml>
+                <grado>2</grado>
+                <nombre>Adjunto</nombre>
+            </_expxml>
+        </root>
+        """)
+        self.temp.close()
 
     def tearDown(self):
-        self.app_context.pop()
+        try:
+            os.remove(self.temp.name)
+        except Exception:
+            pass
 
-    def test_app(self):
-        self.assertIsNotNone(current_app)
-  
-    def test_grado(self):
-        grado = Grado()
-        grado.nombre = 'Ingenieria en Sistemas'
-          
-        self.assertEqual(grado.nombre, 'Ingenieria en Sistemas')
+    def test_importar_grados(self):
+        session = self.Session()
+        import_data(session, self.temp.name, Grado, record_tag="_expxml")
+        session.commit()
 
+        resultados = session.query(Grado).order_by(Grado.grado).all()
+        self.assertEqual(len(resultados), 2)
+        self.assertEqual(resultados[0].grado, 1)
+        self.assertEqual(resultados[0].nombre, "Administración")
+        self.assertEqual(resultados[1].grado, 2)
+        self.assertEqual(resultados[1].nombre, "Adjunto")
+
+        session.close()
 
 if __name__ == '__main__':
     unittest.main()
+      
